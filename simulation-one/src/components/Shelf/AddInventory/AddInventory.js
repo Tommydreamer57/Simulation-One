45d314b4f368a6cae48b6a3b760ad560957533f4
Thomas Lowry <minilao94@yahoo.com>
2017-09-22 14:05:50 -0600

    fixed axios problems

--- simulation-one/src/components/Shelf/AddInventory/AddInventory.js
+++ simulation-one/src/components/Shelf/AddInventory/AddInventory.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './AddInventory.css';
+const url = '/shelfie/';
 
 export default class AddInventory extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            name: '',
+            price: 0
         }
+    }
+    handleNameChange(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    handlePriceChange(e) {
+        this.setState({
+            price: e.target.value
+        })
+    }
+    addToInventory() {
+        axios.post(url, this.state).then(response => response.data)
     }
     render() {
         return (
             <div className="AddInventory" >
+                    <div className="add-bin-name">
+                        <h1>Name</h1>
+                        <input type="text" onChange={e => this.handleNameChange(e)} ></input>
+                    </div>
+                    <div className="bin-price">
+                        <h1>Price</h1>
+                        <input type="text" value={`this.state.price`} onChange={e => this.handlePriceChange(e)} ></input>
+                    </div>
+                    <button onClick={() => this.addToInventory()} >+ Add to Inventory</button>
             </div>
         )
     }
