4e26e672e8313dfd665b215cabda0ed4a511b3d7
jacobcarey93 <31251057+jacobcarey93@users.noreply.github.com>
2017-09-22 13:38:44 -0600

    updates

--- simulation-one/src/components/Shelf/Bin/Bin.js
+++ simulation-one/src/components/Shelf/Bin/Bin.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
 import './Bin.css';
+const url = '/shelfie/'
 
 export default class Bin extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            edit: false,
+            id: '',
+            name: '',
+            price: '',
         }
+    }
+    handleNameChange(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    handlePriceChange(e) {
+        this.setState({
+            price: e.target.value
+        })
+    }
+    save() {
+        axios.put(url + this.state.id)
+            .then(response => response.data)
+            .then(this.setState({
+                edit: false,
+                name: response.data.name,
+                price: response.data.price
+            }))
+    }
+    toggleEditOrSave() {
+        this.state.edit ?
+            save()
+            :
+            this.setState({
+                edit: true
+            })
+    }
+    delete() {
+        axios.delete(url + this.state.id)
     }
     render() {
         return (
             <div className="Bin" >
                 <div className="bin-name">
                     <h1>Name</h1>
-                    <input type="text" placeholder="Name" ></input>
+                    <input type="text" placeholder="Name" onChange={e => this.handleNameChange(e)} ></input>
                 </div>
                 <div className="bin-price">
                     <h1>Price</h1>
-                    <input type="text" value="$0" ></input>
+                    <input type="text" value={this.state.price} onChange={e => this.handlePriceChange(e)} ></input>
                 </div>
-                <button onClick="" >+ Add to Inventory</button>
+                <button onClick={() => this.toggleEditOrSave()} >{this.state.edit ? "Save" : "Edit"}</button>
+                <button onClick={() => this.delete()} >Delete</button>
             </div>
         )
     }
