8bea86fcdf2c78bc66d4487749a634d83cacbc84
Ross Dunford <dunford@Rus-MacBook.local>
2017-09-22 14:36:04 -0600

    index finished

--- simulation-one/server/products_controller.js
+++ simulation-one/server/products_controller.js
@@ -1,9 +1,9 @@
 module.exports = {
     create: (req, res, next) => {
         const dbInstance = req.app.get('db');
-        const {name, price} = req.body;
-        dbInstance.create_product([name, price])
-        .then(() => res.status(200).send([name, price]))
+        const {product, price} = req.body;
+        dbInstance.create_product([product, price])
+        .then(() => res.status(200).send([product, price]))
         .catch(() => res.status(500).send())
     },
 
@@ -13,11 +13,39 @@ module.exports = {
         .then((shelves) => res.status(200).send(shevles))
         .catch(() => res.status(500).send())
         },
-
+        
+    getProduct: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        const {params} = req;
+        dbInstance.get_product(params.product)
+        .then((product) => res.status(200).send(product))
+        .catch(() => res.status(500).send())
+    },
+        
     getBin: (req, res, next) => {
         const dbInstance =req.app.get('db')
         const {params} = req;
         dbInstance.get_bins(params.shelf, params.bin)
-        .then((product) => res.status(200).send(product))
+        .then((bin) => res.status(200).send(bin))
+        .catch(() => res.send(500).send())
+    },
+
+    update: (req,res, next) => {
+        const dbInstance = req.app.get('db')
+        const {params, query} = req;
+
+        dbInstance.update_product([params.product, params.price])
+        .then(() => res.status(200).send())
+        .catch(() => res.status(500).send())
+    },
+
+    delete: (req, res, next) => {
+        dbInstance = req.app.get('db')
+        const {params} = req;
+
+        dbInstance.delete_product([params.shelf, params.bin])
+        .then(()=> res.status(200).send())
+        .catch(()=> res.status(500).send());
+
     }
 }
