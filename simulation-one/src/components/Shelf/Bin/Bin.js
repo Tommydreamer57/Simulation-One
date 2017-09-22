18f64f456c540fb0d18bc485e80c3b28cb1b549c
Thomas Lowry <minilao94@yahoo.com>
2017-09-22 12:26:20 -0600

    pull

--- simulation-one/src/components/Shelf/Bin/Bin.js
+++ simulation-one/src/components/Shelf/Bin/Bin.js
@@ -11,6 +11,15 @@ export default class Bin extends Component {
     render() {
         return (
             <div className="Bin" >
+                <div className="bin-name">
+                    <h1>Name</h1>
+                    <input type="text" placeholder="Name" ></input>
+                </div>
+                <div className="bin-price">
+                    <h1>Price</h1>
+                    <input type="text" value="$0" ></input>
+                </div>
+                <button onClick="" >+ Add to Inventory</button>
             </div>
         )
     }
