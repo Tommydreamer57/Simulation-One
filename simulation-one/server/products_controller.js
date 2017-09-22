b6fcdfe371bbd7f4008bec97bd4a61d20779460b
Ross Dunford <dunford@Rus-MacBook.local>
2017-09-22 14:06:34 -0600

    controller

--- simulation-one/server/products_controller.js
+++ simulation-one/server/products_controller.js
@@ -10,11 +10,14 @@ module.exports = {
     getShelves: (req, res, next) => {
         const dbInstance = req.app.get('db')
         dbInstance.get_shevles()
-        .then((shelves) => res.status(200).send())
+        .then((shelves) => res.status(200).send(shevles))
         .catch(() => res.status(500).send())
         },
 
     getBin: (req, res, next) => {
         const dbInstance =req.app.get('db')
+        const {params} = req;
+        dbInstance.get_bins(params.shelf, params.bin)
+        .then((product) => res.status(200).send(product))
     }
 }
